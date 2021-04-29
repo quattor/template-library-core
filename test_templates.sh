83df01cf95b33a1a2019a450e24be35b7a6967fd
Ward Poelmans <wpoely86@gmail.com>
2021-04-29 12:34:40 +0200

    exclude metaconfig/rsyslog/actions from direct include in tests

--- test_templates.sh
+++ test_templates.sh
@@ -395,6 +395,9 @@ sed -i '/.*metaconfig\/\(elasticsearch\|logstash\|beats\)\/.*_[0-9].*/d' build_t
 # none-versioned ganesha templates are v1
 sed -i "/.*metaconfig\/ganesha\/\(config\(_v1\)\?\|schema\)'/d" build_temp/test.pan
 sed -i "/.*metaconfig\/ganesha\/fsal.*/d" build_temp/test.pan
+# action.pan is included by the schema and can only be included after inputs.pan
+sed -i "/.*metaconfig\/rsyslog\/actions.*/d" build_temp/test.pan
+
 # only spma yum
 sed -i "/.*components\/spma\/\(apt\|ips\).*/d" build_temp/test.pan
 
